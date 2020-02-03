68f010f196f33e5bafd5212d4c50fc387955d1ea
Samantha Meason <sbmeason@gmail.com>
2020-02-03 02:13:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,11 +1,8 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {
         #your key/value pairs here
         },
