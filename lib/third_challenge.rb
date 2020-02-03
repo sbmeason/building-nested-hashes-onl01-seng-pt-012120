f411d71183bf0cd2de7ad78e450791c4ab094847
Samantha Meason <sbmeason@gmail.com>
2020-02-03 02:14:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,9 +8,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {
-        #your key/value pairs here
-        },
+      :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {
         #your key/value pairs here
         },
